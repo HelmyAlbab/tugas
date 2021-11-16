b1d230652518d75224484881924c80d841d72c8c
xSzyy <92864852+xSzyy@users.noreply.github.com>
2021-11-16 09:22:34 +0700

    Update penjualan.blade.php

--- penjualan.blade.php
+++ penjualan.blade.php
@@ -1,1 +1,10 @@
-
+@extends('layoutProject.main')
+@section('content')
+    <h2>Penjualan</h2>
+    <button type="button" class="btn btn-success">Add</button> 
+    <form action="/action_page.php">
+      <label for="gsearch">Search Google:</label>
+      <input type="search" id="gsearch" name="gsearch">
+      <input type="submit">
+    </form>
+@endsection
