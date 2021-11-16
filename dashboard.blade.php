381c5ae16289bad51950f427816f3ec80217ce5e
xSzyy <92864852+xSzyy@users.noreply.github.com>
2021-11-16 09:11:37 +0700

    Add files via upload

---
+++ dashboard.blade.php
@@ -0,0 +1,48 @@
+@extends('layoutProject.main')
+@section('content')
+<div class="row ms-5 me-5">
+    <div class="col-sm-6 mt-4">
+      <div class="card">
+        <div class="card-body" style="background: #3ec85c">
+          <h5 class="card-title">Stok</h5>
+        </div>
+        <div class="card-footer text-muted cardss" style="background: #2c9843">
+          <a href="#" class="btn btn-primary button" style="background: #2c9843">More Info <i class="fas fa-angle-double-right"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 mt-4">
+      <div class="card">
+        <div class="card-body" style="background: #ff6565">
+          <h5 class="card-title">Penjualan</h5>
+        </div>
+        <div class="card-footer text-muted cardss" style="background: #b94747">
+          <a href="#" class="btn btn-primary button" style="background: #b94747">More Info <i class="fas fa-angle-double-right"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row ms-5 me-5">
+    <div class="col-sm-6 mt-4">
+      <div class="card">
+        <div class="card-body" style="background: #525fcf">
+          <h5 class="card-title">Distributor</h5>
+        </div>
+        <div class="card-footer text-muted cardss" style="background: #374096">
+          <a href="#" class="btn btn-primary button" style="background: #374096">More Info <i class="fas fa-angle-double-right"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 mt-4">
+      <div class="card">
+        <div class="card-body" style="background: #ede645">
+          <h5 class="card-title">Kasir</h5>
+        </div>
+        <div class="card-footer text-muted cardss" style="background: #b7c050">
+          <a href="#" class="btn btn-primary button" style="background: #b7c050">More Info <i class="fas fa-angle-double-right"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card-footer ftr">Created by Kelompok 4</div>
+@endsection
